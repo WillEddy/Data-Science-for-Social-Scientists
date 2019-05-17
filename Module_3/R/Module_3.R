f0f1c16f7ee1417f4ad0a03e6d7bb90fc118950a
Eddy <eddyw18@students.ecu.edu>
2019-05-17 11:56:34 -0400

    Test commit

--- Module_3/R/Module_3.R
+++ Module_3/R/Module_3.R
@@ -2,3 +2,5 @@
 library(rstudioapi)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))       #Set working director to where this document is
 raw_df <- read.csv("../data/week3.csv")
+
+# New line
