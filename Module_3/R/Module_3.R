d0c904c1ee8e04964463102edae2916069e371a5
Eddy <eddyw18@students.ecu.edu>
2019-05-17 13:41:29 -0400

    Completed module 3

--- Module_3/R/Module_3.R
+++ Module_3/R/Module_3.R
@@ -1,6 +1,16 @@
 # data import and cleaning
 library(rstudioapi)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))       #Set working director to where this document is
-raw_df <- read.csv("../data/week3.csv")
 
-# New line
+raw_df <- read.csv("../data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[raw_df$timeStart >= as.POSIXct("2017-07-01"),]        #TRIED clean_df <- raw_df$timeStart[grep("2017-06-", raw_df$timeStart)]
+clean_df <- clean_df[clean_df$q6 == "1",]
+
+# Analysis
+clean_df$timeSpent <-  difftime(clean_df$timeEnd,clean_df$timeStart, units="secs")        # ALTERNATIVELY: clean_df$timeSpent <- (unclass(clean_df$timeEnd)) - (unclass(clean_df$timeStart))
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply (clean_df[,5:14], table)
+lapply (frequency_tables_list, barplot)
+sum ((clean_df$q1 >= clean_df$q2) & (clean_df$q2 != clean_df$q3))
